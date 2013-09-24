406c4e72b8ff139910f1f2ba5f5675910009d204
Guilmard Pierre-Jean <guilmardpj@gmail.com>
2013-09-24 12:02:40 +0200

    Space attack - Correct the too many space in script_login

--- script_login.js
+++ script_login.js
@@ -14,30 +14,30 @@ var doLogin = function (req, res, data, col) {
     var u_pwd = param['password'];
 
     users.find({login : u_login}, function (err, docs) {
-								if (err) {
-												console.log('Login error : ' + err);
-												res.writeHead(500, {'Content-Type' : 'text/plain'});
-												res.end('Database is down. Please try again later.');
-								}
-								else if (docs.length <= 0) {
-												res.writeHead(401, {'Content-Type' : 'text/plain' });
-												res.end('User dosen\'t exist');
-								}
-								else if (docs.length > 1) {
-												res.writeHead(409, {'Content-Type' : 'text/plain' });
-												res.end('Database error. Please contact administrator.');
-								}
-								else {
-												if (u_pwd === docs[0].pwd) {
-																res.writeHead(200, {'Content-Type' : 'text/plain' });
-																res.end('Login successfull');
-												}
-												else {
-																res.writeHead(418, {'Content-Type' : 'text/plain' });
-																res.end('Wrong password');
-												}
-								}
-								data.close();
+	if (err) {
+	    console.log('Login error : ' + err);
+	    res.writeHead(500, {'Content-Type' : 'text/plain'});
+	    res.end('Database is down. Please try again later.');
+	}
+	else if (docs.length <= 0) {
+	    res.writeHead(401, {'Content-Type' : 'text/plain' });
+	    res.end('User dosen\'t exist');
+	}
+	else if (docs.length > 1) {
+	    res.writeHead(409, {'Content-Type' : 'text/plain' });
+	    res.end('Database error. Please contact administrator.');
+	}
+	else {
+	    if (u_pwd === docs[0].pwd) {
+		res.writeHead(200, {'Content-Type' : 'text/plain' });
+		res.end('Login successfull');
+	    }
+	    else {
+		res.writeHead(418, {'Content-Type' : 'text/plain' });
+		res.end('Wrong password');
+	    }
+	}
+	data.close();
     });
 }
 
